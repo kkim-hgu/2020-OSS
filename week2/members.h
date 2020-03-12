466697984f3aaf3b831f408047edcab93d0aa1c0
Kwang Kim <37907971+kkim-hgu@users.noreply.github.com>
2020-03-12 22:05:17 +0900

    Create members.h

---
+++ week2/members.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_MEMBERS 10      // 저장할 수 있는 회원 레코드 최대 갯수
+
+// 회원 정보 레코드의 데이터 타입을 정의함
+typedef struct st_member{
+    char name[20];  // 회원이름
+    char phone[20]; // 회원전화번호
+    int birthyear;  // 회원생년
+    char city[20];  // 회원거주도시
+} T_Record;
+
+// 라이브러리 함수 원형을 선언함
+int m_is_available();       // 현재 추가할 수 있는 데이터 공간이 있는가?
+int m_first_available();    // 추가할 수 있는 가장 빠른 포인터 배열의 인덱스 찾기
+int m_count();              // 현재 저장된 회원 레코드의 갯수 가져오기
+void m_create(char* n, char* ph, int b, char* c);   // 회원정보로 신규 회원 레코드 추가
+T_Record* m_search_by_name(char* n);                // 이름이 일치하는 회원 레코드 포인터 찾기
+void m_update(T_Record* p, char* ph, int b, char* c);  // 특정 회원 레코드의 정보 업데이트
+void m_delete(T_Record* p);     // 특정 회원 레코드의 회원정보 제거
+void m_get_all(T_Record* a[]);  // 회원정보가 들어있는 모든 레코드 포인터의 배열을 만들기
+char* m_to_string(T_Record* p); // 특정 회원 레코드의 내용을 문자열로 가져오기
+char* m_getname(T_Record* p);   // 특정 회원 레코드의 회원이름 가져오기
+char* m_getphone(T_Record* p);  // 특정 회원 레코드의 회원전화번호 가져오기
+int m_getbirthyear(T_Record* p);// 특정 회원 레코드의 회원생년 가져오기
+char* m_getcity(T_Record* p);   // 특정 회원 레코드의 회원거주도시 가져오기
